f6e153404edc1da6b1694e7c680c5e1236a1eb96
Eduardo Marques <eduardomarques.dev01@gmail.com>
2023-08-23 21:16:12 -0300

    desenvolvendo a forca
    finzaliando

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import random
 
-palavras = ["python", "elefante", "graveto", "computador", "programação", "biscoito", "laranja", "headset", "messi"]
+palavras = ["python", "professor", "messi", "programação", "headset", "queijo", "laranja", "televisão", "elefante"]
 
 def escolha_palavra():
     return random.choice(palavras)
 
 def jogar_focar(palavra):
-    palavra_secreta = palavra
-    letras_certas = []
+    palavras_secreta = palavra
+    letras_certa = []
     tentativas = 6
+
 
     while True:
         letra = input('Digite uma letra: ').lower()
 
-        if letra in palavra_secreta:
-            letras_certas.append(letra)
+        if letra in palavras_secreta:
+            letras_certa.append(letra)
+        else:
+            print(f"Letra incorreta! Você tem {tentativas} tentativas restantes ")
+
+        palavra_exibidia = " "
+        for letra_secreta in palavras_secreta:
+            if letra_secreta in letras_certa:
+                palavra_exibidia += letra_secreta + " "
+
+            else:
+                palavra_exibidia += "_ "
+
+        print(palavra_exibidia)
+
+        if palavra_exibidia.replace(" ", "") == palavras_secreta:
+            print("Parabéns, você acertou a palavra!")
+            break
+
+        if tentativas == 0:
+            print(f"Fim de jogo! A palavra era '{palavras_secreta}'.")
+
+
